6632348413084cd2f53277fe8e57378b1f1f6829
Ochola Alfred <59102788+aysh-ug@users.noreply.github.com>
2021-08-14 01:03:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+function ai() {
+       document.getElementById("ai").style.display = "block";
+       document.getElementById("ps").style.display = "none";
+       document.getElementById("web").style.display = "none";
+       document.getElementById("java").style.display = "none";
+       document.getElementById("btns").style.display = "none";
+}
+
+
+function aic() {
+       document.getElementById("ai").style.display = "none";
+       document.getElementById("btns").style.display = "block";
+}
+
+function ps() {
+    document.getElementById("ps").style.display = "block";
+    document.getElementById("ai").style.display = "none";
+       document.getElementById("web").style.display = "none";
+       document.getElementById("java").style.display = "none";
+       document.getElementById("btns").style.display = "none";
+}
+
+
+function psc() {
+    document.getElementById("ps").style.display = "none";
+    document.getElementById("btns").style.display = "block";
+}
+
+
+function web() {
+    document.getElementById("web").style.display = "block";
+    document.getElementById("ai").style.display = "none";
+       document.getElementById("ps").style.display = "none";
+       document.getElementById("java").style.display = "none";
+       document.getElementById("btns").style.display = "none";
+}
+
+
+function wc() {
+    document.getElementById("web").style.display = "none";
+    document.getElementById("btns").style.display = "block";
+}
+
+
+function java() {
+    document.getElementById("java").style.display = "block";
+    document.getElementById("ai").style.display = "none";
+       document.getElementById("ps").style.display = "none";
+       document.getElementById("web").style.display = "none";
+       document.getElementById("btns").style.display = "none";
+}
+
+
+function jc() {
+    document.getElementById("java").style.display = "none";
+    document.getElementById("btns").style.display = "block";
+}
